5b98b9af2aaf6152de6830a073fed2287ce0e6e2
Joe Nibali <joenibali27@gmail.com>
2021-05-30 14:40:52 -0700

    2nd commit
    Wrote code for fizzbuzz function. added additional tests for all words, Tests passed

--- buzz.py
+++ buzz.py
@@ -1,2 +1,17 @@
 def fizz():
-   pass
+    for x in range(1,100):
+        output = ""
+        if(x % 3 == 0 and x % 5 == 0):
+            output + ("FizzBuzz")
+        elif(x % 5 == 0):
+            output + ("Buzz")
+        elif(x % 3 == 0):
+            output + ("Fizz")
+        else:
+            output + str(x)
+    return output
+       
+
+
+
+
--- buzz_test.py
+++ buzz_test.py
@@ -4,6 +4,8 @@ import buzz
 class TestCase(unittest.TestCase):
     def test1(self):
         self.assertIn(buzz.fizz(), 'Fizz')
+        self.assertIn(buzz.fizz(), 'Buzz')
+        self.assertIn(buzz.fizz(), 'FizzBuzz')
 
 if __name__ == '__main__':
     unittest.main()
